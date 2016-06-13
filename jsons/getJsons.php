baaa4e79172d73c2b497b55360cfecf90f2a2650
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-06-13 08:40:27 -0300

    usa o MapasSDK para pegar os eventos da virada

--- jsons/getJsons.php
+++ jsons/getJsons.php
@@ -1,71 +1,126 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
 
-if (file_exists(__DIR__ . '/api-config.php')) {
-    include __DIR__ . '/api-config.php';
-}
-if(!defined('API_URL')) define('API_URL', "http://mapasculturais.hacklab.com.br/api/");
+require __DIR__ . '/MapasSDK/vendor/autoload.php';
+require __DIR__ . '/api-config.php';
 
-if(!defined('PROJECT_ID')) define('PROJECT_ID', 4);
+use MapasSDK as sdk;
 
-if(!defined('REPLACE_IMAGES_URL_FROM')) define('REPLACE_IMAGES_URL_FROM', 'http://mapasculturais.hacklab.com.br//files/');
+$mapas = new MapasSDK\MapasSDK(MAPAS_URL, API_PUBLIC_KEY, API_PRIVATE_KEY);
 
-if(!defined('REPLACE_IMAGES_URL_TO')) define('REPLACE_IMAGES_URL_TO', 'http://viradacultural.prefeitura.sp.gov.br/imagens/');
+$mapas->debugRequest = SDK_DEBUG_REQUEST;
+$mapas->debugResponse = SDK_DEBUG_RESPONSE;
 
-$get_spaces_url = API_URL . "space/findByEvents?@select=id,name,shortDescription,endereco,location&@files=(avatar.viradaSmall,avatar.viradaBig):url&@order=name&@keyword=virada%20cultural%202014&@from=2014-05-17&@to=2014-05-18";
-$get_events_url = API_URL . "event/find?@select=id,name,shortDescription,description,classificacaoEtaria,terms,traducaoLibras,descricaoSonora&@files=(avatar.viradaSmall,avatar.viradaBig):url&project=EQ(@Project:" . PROJECT_ID . ")";
+$project_id = PROJECT_ID;
 
-echo "\nbaixando eventos $get_events_url\n\n";
-$events_json = file_get_contents($get_events_url);
+$date_from = DATE_FROM;
+$date_to = DATE_TO;
 
-echo "\nbaixando espaços $get_spaces_url\n\n";
-$spaces_json = file_get_contents($get_spaces_url);
+$project_ids = $mapas->getChildrenIds('project', PROJECT_ID, true);
 
-$spaces = json_decode($spaces_json);
-$events = array();
+echo "\nbaixando eventos...";
+$events = $mapas->findEntities(
+        'event', 
+        'id,name,subTitle,shortDescription,description,classificacaoEtaria,terms,traducaoLibras,descricaoSonora,project.id,project.name,project.singleUrl', 
+        [
+            '@files' => '(avatar.viradaSmall,avatar.viradaBig):url',
+            'project' => sdk\IN($project_ids),
+            '@permissions' => 'view'
+        ]);
+
 $events_by_id = array();
 
-foreach (json_decode($events_json) as $e) {
-    $events[] = $e;
+$event_ids = [];
+
+foreach ($events as $e) {
     $events_by_id[$e->id] = $e;
     $event_ids[] = $e->id;
 }
 
-$event_ids = implode(',', $event_ids);
-
-$occurrences_json = file_get_contents(API_URL . "eventOccurrence/find?@select=id,space.id,eventId,rule&event=IN($event_ids)&@order=_startsAt");
-
-$occurrences = json_decode($occurrences_json);
-
 $result_events = array();
 
-$count = 0;
-foreach ($occurrences as $occ) {
-    $rule = $occ->rule;
-    $e = $events_by_id[$occ->eventId];
-    $e->spaceId = $occ->space->id;
-    $e->startsAt = $rule->startsAt;
-    $e->startsOn = $rule->startsOn;
-    $e->duration = @$rule->duration;
-    $e->acessibilidade = array();
-    if($e->traducaoLibras)
-        $e->acessibilidade[] = 'Tradução para LIBRAS';
+$spaces = [];
+$space_ids = [];
+if($event_ids){
 
-    if($e->descricaoSonora)
-        $e->acessibilidade[] = 'Descrição sonora';
+    //"eventOccurrence/find?@select=id,space.id,eventId,rule&event=IN($event_ids)&@order=_startsAt"
+    $occurrences = $mapas->findEntities(
+            'eventOccurrence',
+            'id,space.id,eventId,rule,space.id',
+            [
+                'event' => sdk\IN($event_ids),
+                '@order' => '_startsAt',
+                '@permissions' => 'view'
+            ]);
+    
+    $space_ids = [];
+    
+    $count = 0;
+    foreach ($occurrences as $occ) {
+        $rule = $occ->rule;
+        $e = clone $events_by_id[$occ->eventId];
+        $e->id = $occ->id;
+        $e->eventId =  $occ->eventId;
+        
+        if(!in_array($occ->space->id, $space_ids)){
+            $space_ids[] = $occ->space->id;
+        }
+
+        $e->spaceId = $occ->space->id;
+        $e->startsAt = $rule->startsAt;
+        $e->startsOn = $rule->startsOn;
+
+        $datetime = new DateTime("{$rule->startsOn} {$rule->startsAt}");
+
+        $e->price = $rule->price;
+
+        $e->timestamp = $datetime->getTimestamp();
+
+        $e->duration = @$rule->duration;
+
+        if($e->duration == 1440){
+            $e->duration = '24h00';
+        }
+
+        $e->acessibilidade = array();
+        if($e->traducaoLibras)
+            $e->acessibilidade[] = 'Tradução para LIBRAS';
+
+        if($e->descricaoSonora)
+            $e->acessibilidade[] = 'Descrição sonora';
 
 
-    $small_image_property = '@files:avatar.viradaSmall';
-    $big_image_property = '@files:avatar.viradaBig';
+        $small_image_property = '@files:avatar.viradaSmall';
+        $big_image_property = '@files:avatar.viradaBig';
 
-    if (property_exists($e, $small_image_property)) {
-        $e->defaultImage = str_replace(REPLACE_IMAGES_URL_FROM, REPLACE_IMAGES_URL_TO, $e->$big_image_property->url);
-        $e->defaultImageThumb = str_replace(REPLACE_IMAGES_URL_FROM, REPLACE_IMAGES_URL_TO, $e->$small_image_property->url);
-    } else {
-        $e->defaultImage = '';
-        $e->defaultImageThumb = '';
+        if (property_exists($e, $small_image_property)) {
+            $e->defaultImage = str_replace(REPLACE_IMAGES_URL_FROM, REPLACE_IMAGES_URL_TO, $e->$big_image_property->url);
+            $e->defaultImageThumb = str_replace(REPLACE_IMAGES_URL_FROM, REPLACE_IMAGES_URL_TO, $e->$small_image_property->url);
+            $e->image768 = str_replace(REPLACE_IMAGES_URL_FROM, REPLACE_IMAGES_URL_TO, $e->$small_image_property->url);
+            $e->image800 = str_replace(REPLACE_IMAGES_URL_FROM, REPLACE_IMAGES_URL_TO, $e->$big_image_property->url);
+            $e->image1024 = str_replace(REPLACE_IMAGES_URL_FROM, REPLACE_IMAGES_URL_TO, $e->$big_image_property->url);
+            $e->image1280 = str_replace(REPLACE_IMAGES_URL_FROM, REPLACE_IMAGES_URL_TO, $e->$big_image_property->url);
+        } else {
+            $e->defaultImage = '';
+            $e->defaultImageThumb = '';
+        }
+
+        $result_events[] = $e;
     }
     
-    $result_events[] = $e;
+    if($space_ids){
+        echo "\nbaixando espaços...";
+        $spaces = $mapas->findEntities(
+            'space',
+            'id,name,shortDescription,endereco,location', 
+            [
+                '@files' => '(avatar.viradaSmall,avatar.viradaBig):url',
+                '@order' => 'name',
+                '@permissions' => 'view',
+                'id' => sdk\IN($space_ids)
+            ]);
+    }
+
 }
 
 file_put_contents(__DIR__ . '/events.json', json_encode($result_events));
